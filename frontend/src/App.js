91eaced4274267e6640c031a89ebd3db34d4d7c8
Bhaskar Joshi <bhaskerjoshi977@gmail.com>
2024-02-25 15:26:38 +0530

    offers,shopCategort,loginsignup and context page done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,9 @@ import Product from "./Pages/Product";
 import Cart from "./Pages/Cart";
 import LoginSignup from "./Pages/LoginSignup";
 import Footer from "./Components/Footer/Footer";
+import men_banner from "./Components/Assets/banner_mens.png";
+import women_banner from "./Components/Assets/banner_women.png";
+import kid_banner from "./Components/Assets/banner_kids.png";
 
 function App() {
   return (
@@ -16,15 +19,15 @@ function App() {
           <Route path="/" element={<Shop />}></Route>
           <Route
             path="/mens"
-            element={<ShopCategory category="mens" />}
+            element={<ShopCategory banner={men_banner} category="men" />}
           ></Route>
           <Route
             path="/womens"
-            element={<ShopCategory category="womens" />}
+            element={<ShopCategory banner={women_banner} category="women" />}
           ></Route>
           <Route
             path="/kids"
-            element={<ShopCategory category="kids" />}
+            element={<ShopCategory banner={kid_banner} category="kid" />}
           ></Route>
           <Route path="/product" element={<Product />}></Route>
           <Route path=":productId" element={<Product />}></Route>
